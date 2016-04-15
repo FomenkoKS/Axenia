ea8e553a7f373586856767d160056dabcd025606
Alexander Ivanov <Alexander.Ivanov@aplana.com>
2016-04-15 18:27:48 +0500

    fix

--- bot/config.php
+++ bot/config.php
@@ -1,9 +1,9 @@
 <?php
-define("MYSQL_USER",	"user3126_abrikos");
-define("MYSQL_PASSWORD","N7m4Z0b0");
-define("MYSQL_DATABASE","user3126_anfisa");
-define("BOT_NAME","Axenia_Bot");
-define('BOT_TOKEN', '154948147:AAHMcfRccPy6fF8gSyBe9V_6xTxTjq5MZR4');
+define("MYSQL_USER",	"");
+define("MYSQL_PASSWORD","");
+define("MYSQL_DATABASE","");
+define("BOT_NAME","");
+define('BOT_TOKEN', '');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
-define('PATH_TO_SITE', 'http://axeni.ru/');
+define('PATH_TO_SITE', '');
 ?>
