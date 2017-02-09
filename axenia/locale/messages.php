fb0cded39b5fe4bf0e9de70cb5fb5ae13a665f90
Alexander Ivanov <Alexander.Ivanov@aplana.com>
2017-02-09 22:33:04 +0500

    fix top \r

--- axenia/locale/messages.php
+++ axenia/locale/messages.php
@@ -99,9 +99,9 @@ Also I understand messages in format like \"+ @username\" and etc.\r\n
         ),
     "karma.top.title" =>
         array(
-            "en" => "<b>🏆Top list of Karma owners in the «:chatName»:</b>\r\n",
-            "ru" => "<b>🏆Самые почётные люди групы «:chatName»:</b>\r\n",
-            "ruUN" => "<b>🏆Самые длинные кармописюны группы «:chatName»:</b>\r\n"
+            "en" => "<b>🏆Top list of Karma owners in the «:chatName»:</b>\r\n\r\n",
+            "ru" => "<b>🏆Самые почётные люди групы «:chatName»:</b>\r\n\r\n",
+            "ruUN" => "<b>🏆Самые длинные кармописюны группы «:chatName»:</b>\r\n\r\n"
         ),
     "karma.top.row" =>
         array(
