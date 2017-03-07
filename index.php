6f17df17f667f4fe0418031143f3f5aa80d7aac6
Alexander Ivanov <Alexander.Ivanov@aplana.com>
2017-03-07 12:42:58 +0500

    отступ от кнопки пожертсовавния

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ $type = $site->getViewType($_GET);
                 <button class="btn btn-sm btn-dark" data-toggle="modal" data-target="#myModal" id="for_food">
                     Пожертвовать на проект
                 </button>
-                <p>Написать автору <strong class="tg_user wrote">abrikos</strong><br><a
+                <p style="padding-top: 4px;" >Написать автору <strong class="tg_user wrote">abrikos</strong><br><a
                         href="https://telegram.me/storebot?start=Axenia_bot">Оставить отзыв</a></p>
             </div>
             <div class="col-md-6 col-xs-6 text-right">
