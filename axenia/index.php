84e8499f715c45ed99bec11071b068367e7f7bfd
abrikoseg <Tgh6JKrt>
2017-05-05 15:23:18 +0500

    Добавление класса для взаимодействия с Redis, небольшой анти-спам скрипт

--- axenia/index.php
+++ axenia/index.php
@@ -11,6 +11,7 @@ require_once('logic/BotDao.php');
 require_once('logic/BotService.php');
 require_once('logic/Axenia.php');
 require_once('logic/ShortUrl.php');
+require_once('logic/Redis.php');
 
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
@@ -18,12 +19,22 @@ $update = json_decode($content, true);
 if (!$update) {
     exit;
 } else {
-    // save incoming requset
-    //file_put_contents("array.txt", print_r($update, true));
-    Request::setUrl(API_URL);
-    $bot = new Axenia(new BotService(new BotDao()));
-
-    $bot->handleUpdate($update);
+    $redis=new Redis();
+    function redis_error($error) {
+        Request::sendMessage(LOG_CHAT_ID,$error);
+        throw new error($error);
+    }
+    $redis->connect('127.0.0.1', 6379);
+    $key="from:".$update['message']['from']['id'];
+    if(!isset($update['message']['forward_from']))$redis->incr($key);
+    $count=$redis->get($key);
+    if($count==1) $redis->expire($key,10);
+    if($count<10){
+        Request::setUrl(API_URL);
+        $bot = new Axenia(new BotService(new BotDao()));
+        $bot->handleUpdate($update);
+    }
+    $redis->close();
 }
 
 
