a67352513db4b78dfbbbefdb22e9cf69786bcb66
Fomenko-KS <rik.pie@gmail.com>
2022-05-31 21:55:21 +0500

    Separate with site

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="mastfoot mt-auto pt-3">
-        <div class="inner">
-          <p>Support: <a href="tg://resolve?domain=axeniasupport">@AxeniaSupport</a></p>
-        </div>
-      </footer>
