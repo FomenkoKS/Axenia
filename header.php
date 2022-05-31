33049429003bf9bb6364d91c4eed0b3c5e42432b
Fomenko-KS <rik.pie@gmail.com>
2022-05-31 22:11:06 +0500

    Separate with site

--- header.php
+++
@@ -1,6 +0,0 @@
-
-      <header class="masthead p-3">
-        <div class="inner">
-          <h3 class="masthead-brand">@Axenia_bot</h3>
-        </div>
-      </header>
--- updStats.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("SiteService.php");
-$site = new SiteService();
-$type = $site->updateStats($site->getStats());
-?>
